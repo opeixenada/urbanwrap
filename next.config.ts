05e42f39908ee1dd0b5a8732277583baae098255
Anna Yudina <annayudi@gmail.com>
2024-12-13 21:54:06 +0100

    Fix image configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
+
+const isGithubPages = process.env.NEXT_PUBLIC_GITHUB_PAGES || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubPages) {
+    assetPrefix = `/urbanwrap/`
+    basePath = `/urbanwrap`
+}
 
 const nextConfig: NextConfig = {
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/urbanwrap' : '',
+    output: "export",
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     images: {
         unoptimized: true,
     },
